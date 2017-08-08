b8cb47ea9a23164a678f2fe5f7d3a96cbd12d869
olivia_staybloomy <chen3461@umn.edu>
2017-08-08 17:26:47 -0500

    Added contributors info

--- forest/primitives/Primitives.py
+++ forest/primitives/Primitives.py
@@ -2,7 +2,8 @@
 Copyright (c) 2017 Eric Shook. All rights reserved.
 Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
 @author: eshook (Eric Shook, eshook@gmail.edu)
-@contributors: Luyi C. Hunter, Xinran Duan
+@contributors: (Luyi Hunter, chen3461@umn.edu; Xinran Duan, duanx138@umn.edu)
+ @contributors: <Contribute and add your name here!>
 """
 
 import rasterio
